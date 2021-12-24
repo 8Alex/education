6a07038d0360a733784a390b0622d1dd1fe174e9
Alexandra <bor-18@mail.ru>
2021-12-24 15:21:51 +0300

    first commit

---
+++ week13/calculator.js
@@ -0,0 +1,27 @@
+function calcSum() 
+{
+    let a = +prompt("Первое число?");
+    let b = +prompt("Второе число?");
+    alert(`${a} + ${b} = ${a+b}`);
+}
+
+function calcSub() 
+{
+    let a = +prompt("Первое число?");
+    let b = +prompt("Второе число?");
+    alert(`${a} - ${b} = ${a-b}`);
+}
+
+function calcMul() 
+{
+    let a = +prompt("Первое число?");
+    let b = +prompt("Второе число?");
+    alert(`${a} * ${b} = ${a*b}`);
+}
+
+function calcDiv() 
+{
+    let a = +prompt("Первое число?");
+    let b = +prompt("Второе число?");
+    alert(`${a} / ${b} = ${a/b}`);
+}
