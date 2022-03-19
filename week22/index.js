cf73d15b9b08c0bdd8408e758de812a2aeb53142
Alexandra <bor-18@mail.ru>
2022-03-19 16:43:48 +0300

    first commit

---
+++ week22/index.js
@@ -0,0 +1,40 @@
+getElementValue = (id) => {
+    return Number(document.getElementById(id).value);
+}
+
+function calcSum() {
+    const number1 = getElementValue("number1");
+    const number2 = getElementValue("number2");
+    const result = number1 + number2;
+    document.getElementById("resultSum").value = result;
+}
+
+function calcSub() {
+    const number3 = getElementValue("number3");
+    const number4 = getElementValue("number4");
+    const result = number3 - number4;
+    document.getElementById("resultSub").value = result;
+}
+
+function calcMul() {
+    const number5 = getElementValue("number5");
+    const number6 = getElementValue("number6");
+    const result = number5 * number6;
+    document.getElementById("resultMul").value = result;
+}
+
+function calcDiv() {
+    const number7 = getElementValue("number7");
+    const number8 = getElementValue("number8");
+
+    try {
+        if (number8 === 0) {
+            throw new Error("На ноль делить нельзя!");
+        }
+    } catch (e) {
+        alert(e.message);
+    }
+
+    const result = number7 / number8;
+    document.getElementById("resultDiv").value = result;
+}
